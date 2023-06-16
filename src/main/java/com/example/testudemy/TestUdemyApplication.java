f6512c3d9aaf652c4056de40ec995edf9e464293
jvavieira <joaovictoravtrabs@gmail.com>
2023-06-15 21:47:05 -0300

    // First Commit

---
+++ src/main/java/com/example/testudemy/TestUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.example.testudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestUdemyApplication.class, args);
+	}
+
+}
